9b998efc8aa88fcc141b7b690e80d7db2e00ad5f
Linkrw12 <robert_w_link@yahoo.com>
2022-08-27 11:45:00 -0400

    mass commit

--- utils/pubmed.py
+++ utils/pubmed.py
@@ -1,14 +1,42 @@
 from Bio import Entrez
+import re
+
 
 class PubMedAbstract(object):
-    def __init__(self):
-        pass  
+    """Parses an individual abstract isolated from 
+    the NCBI PubMed summary file. It's a container that
+    holds relevent information for downstream clustering,
+    such as title, date, and authors. Its main function 
+    is to split the abstract into individual words that 
+    will be used for the tokenizer. 
+    
+    PubMedAbstract is not meant to be used by itself, but 
+    is meant to be initialized through one of the 
+    PubMedAbstractCollection class methods. 
+    """
+    def __init__(self, raw_abstract_str: str):
 
-    def x(self):
-        pass
+        self.raw_abstract_str = raw_abstract_str
+        
+        split_abstract_str = self.raw_abstract_str.split("\n\n")
+        
+        self.title = split_abstract_str[1].replace("\n", "")
+        # self.date
+        self.journal = split_abstract_str[0]
+        self.authors = ''
+        self.abstract_str = split_abstract_str[4].replace("\n", "")
+        self.doi = re.search("DOI: (.+)", raw_abstract_str).group(1)
+        self.pmcid = re.search("PMCID: (.+)", raw_abstract_str).group(1)
+        self.pmid = int(re.search("PMID: (\d+)", raw_abstract_str).group(1))
+        
+    def split_abstract_into_words(self):
+        return self.abstract.split(" ")
+
     
 class PubMedAbstractCollection(object):
-    def __init__(self, abstracts):
+    """
+    """
+    def __init__(self, abstracts: list[PubMedAbstract]):
         self.abstracts = abstracts
     
     @classmethod
@@ -18,3 +46,7 @@ class PubMedAbstractCollection(object):
     @classmethod
     def load_from_abstract_txt_file(cls, file):
         pass
+    
+    def split_abstracts_into_words(self):
+        return [abstract.split_abstract_into_words()
+                for abstract in self.abstracts]
