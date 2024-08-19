eccc89bcb786f1fc03d02b17bf93506b272767bd
Linkrw12 <robert_w_link@yahoo.com>
2024-08-19 17:20:07 -0400

    linted using black

--- utils/pubmed.py
+++ utils/pubmed.py
@@ -3,50 +3,50 @@ import re
 
 
 class PubMedAbstract(object):
-    """Parses an individual abstract isolated from 
+    """Parses an individual abstract isolated from
     the NCBI PubMed summary file. It's a container that
     holds relevent information for downstream clustering,
-    such as title, date, and authors. Its main function 
-    is to split the abstract into individual words that 
-    will be used for the tokenizer. 
-    
-    PubMedAbstract is not meant to be used by itself, but 
-    is meant to be initialized through one of the 
-    PubMedAbstractCollection class methods. 
+    such as title, date, and authors. Its main function
+    is to split the abstract into individual words that
+    will be used for the tokenizer.
+
+    PubMedAbstract is not meant to be used by itself, but
+    is meant to be initialized through one of the
+    PubMedAbstractCollection class methods.
     """
+
     def __init__(self, raw_abstract_str: str):
 
         self.raw_abstract_str = raw_abstract_str
-        
+
         split_abstract_str = self.raw_abstract_str.split("\n\n")
-        
+
         self.title = split_abstract_str[1].replace("\n", "")
         # self.date
         self.journal = split_abstract_str[0]
-        self.authors = ''
+        self.authors = ""
         self.abstract_str = split_abstract_str[4].replace("\n", "")
         self.doi = re.search("DOI: (.+)", raw_abstract_str).group(1)
         self.pmcid = re.search("PMCID: (.+)", raw_abstract_str).group(1)
         self.pmid = int(re.search("PMID: (\d+)", raw_abstract_str).group(1))
-        
+
     def split_abstract_into_words(self):
         return self.abstract.split(" ")
 
-    
+
 class PubMedAbstractCollection(object):
-    """
-    """
+    """ """
+
     def __init__(self, abstracts: list[PubMedAbstract]):
         self.abstracts = abstracts
-    
+
     @classmethod
     def load_from_search_query(cls, query):
         pass
-    
+
     @classmethod
     def load_from_abstract_txt_file(cls, file):
         pass
-    
+
     def split_abstracts_into_words(self):
-        return [abstract.split_abstract_into_words()
-                for abstract in self.abstracts]
+        return [abstract.split_abstract_into_words() for abstract in self.abstracts]
--- utils/widgets.py
+++ utils/widgets.py
@@ -6,34 +6,38 @@ from .pubmed import PubMedAbstractCollection
 def load_abstract_widgets():
     input_method = st.selectbox(
         "Search PubMed or load previously generated abstract file:",
-        ('Search PubMed', 'Load Generated Abstract File')
+        ("Search PubMed", "Load Generated Abstract File"),
     )
-    if input_method == 'Search PubMed':
+    if input_method == "Search PubMed":
         query = st.text_input("PubMed Query:")
         abstracts = PubMedAbstractCollection.load_from_search_query(query)
-    elif input_method == 'Load Generated Abstract File':
+    elif input_method == "Load Generated Abstract File":
         file = st.file_uploader("Upload Pubmed Abstract (set) file:")
         abstracts = PubMedAbstractCollection.load_from_abstract_txt_file(file)
     return abstracts
 
+
 def select_transformers_widgets():
-    
     def load_embedder_and_tokenizer():
         pass
-    
+
     embedder_tokenizer_combo = st.selectbox(
         "Select Pubmed Abstract Embedder and Tokenizer:",
-        ('microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext', 'other Hugging Face transformer', 'custom pretrained transformer')
+        (
+            "microsoft/BiomedNLP-PubMedBERT-base-uncased-abstract-fulltext",
+            "other Hugging Face transformer",
+            "custom pretrained transformer",
+        ),
     )
-    
-    if embedder_tokenizer_combo == 'custom pretrained transformer':
+
+    if embedder_tokenizer_combo == "custom pretrained transformer":
         embedder = st.file_uploader("Embeder")
         tokenizer = st.file_uploader("Tokenizer")
         return embedder, tokenizer
-    elif embedder_tokenizer_combo == 'other Hugging Face transformer':
+    elif embedder_tokenizer_combo == "other Hugging Face transformer":
         hugging_face_model_card = st.text_input("Type model card")
-        embedder, tokenizer = 'foo', 'bar'
+        embedder, tokenizer = "foo", "bar"
         return embedder, tokenizer
     else:
-        embedder, tokenizer = 'foo', 'bar'
+        embedder, tokenizer = "foo", "bar"
         return embedder, tokenizer
